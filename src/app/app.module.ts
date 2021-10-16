d8e120b921a50099c2c540997ef0a7c2bd99a57b
Giuseppe Gigliotti <giuseppegigliottidev@gmail.com>
2021-10-16 22:14:11 +0200

    added header and sidebar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,26 @@ import { AppComponent } from './app.component';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
 
+import { RemixIconModule } from "angular-remix-icon";
+import { RiNotificationLine, RiChat1Line, RiSearch2Line, RiAlignCenter, RiDashboard3Line, RiServerLine, RiArrowDownSLine, RiTableLine, RiBarChart2Line, RiAncientGateLine, RiUserLine, RiInformationLine, RiMailSendLine, RiContactsBook2Line, RiLoginCircleLine, RiForbidLine, RiFileLine, RiCheckboxBlankCircleLine } from 'angular-remix-icon';
+
+import { HeaderComponent } from './components/partials/header/header.component';
+import { SidebarComponent } from './components/partials/sidebar/sidebar.component';
+
+const icons = {
+  RiNotificationLine, RiChat1Line, RiSearch2Line,RiAlignCenter, RiDashboard3Line,RiServerLine,RiArrowDownSLine, RiTableLine, RiBarChart2Line, RiAncientGateLine, RiUserLine, RiInformationLine, RiMailSendLine, RiContactsBook2Line, RiLoginCircleLine, RiForbidLine, RiFileLine, RiCheckboxBlankCircleLine          
+};
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    RemixIconModule.configure(icons),
     ServiceWorkerModule.register('ngsw-worker.js', {
       enabled: environment.production,
       // Register the ServiceWorker as soon as the app is stable

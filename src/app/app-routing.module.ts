67681471366f4ae298155600d3f797a77d2d9d67
Giuseppe Gigliotti <giuseppegigliottidev@gmail.com>
2021-10-20 22:48:53 +0200

    started project with tailwind starter kit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+//! layouts
+import { MainComponent } from './layouts/main/main.component';
+//! Components
+import { DashboardComponent } from "./views/dashboard/dashboard.component"
+const routes: Routes = [
+    //! dashboard
+  {
+    path: "dashboard",
+    component: MainComponent,
+    children: [
+      { path: "dashboard", component: DashboardComponent },
+      // { path: "settings", component: SettingsComponent },
+      // { path: "tables", component: TablesComponent },
+      // { path: "maps", component: MapsComponent },
+      { path: "", redirectTo: "dashboard", pathMatch: "full" },
+    ],
+  },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,33 +3,31 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ServiceWorkerModule } from '@angular/service-worker';
-import { environment } from '../environments/environment';
 
-import { RemixIconModule } from "angular-remix-icon";
-import { RiComputerLine, RiNotificationLine,RiCompassLine, RiChat1Line, RiSearch2Line, RiAlignCenter, RiDashboard3Line, RiServerLine, RiArrowDownSLine, RiTableLine, RiBarChart2Line, RiAncientGateLine, RiUserLine, RiInformationLine, RiMailSendLine, RiContactsBook2Line, RiLoginCircleLine, RiForbidLine, RiFileLine, RiCheckboxBlankCircleLine } from 'angular-remix-icon';
-
+//* partials
 import { NavbarComponent } from './components/partials/navbar/navbar.component';
-
-const icons = {
-  RiComputerLine, RiNotificationLine,RiCompassLine, RiChat1Line, RiSearch2Line,RiAlignCenter, RiDashboard3Line,RiServerLine,RiArrowDownSLine, RiTableLine, RiBarChart2Line, RiAncientGateLine, RiUserLine, RiInformationLine, RiMailSendLine, RiContactsBook2Line, RiLoginCircleLine, RiForbidLine, RiFileLine, RiCheckboxBlankCircleLine          
-};
+import { SidebarComponent } from './components/partials/sidebar/sidebar.component';
+//* components
+import { HomeStatsComponent } from './components/home/home-stats/home-stats.component';
+import { CardsComponent } from './components/shared/cards/cards.component';
+//* layouts
+import { MainComponent } from './layouts/main/main.component';
+//* views
+import { DashboardComponent } from './views/dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    SidebarComponent,
+    HomeStatsComponent,
+    CardsComponent,
+    DashboardComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    RemixIconModule.configure(icons),
-    ServiceWorkerModule.register('ngsw-worker.js', {
-      enabled: environment.production,
-      // Register the ServiceWorker as soon as the app is stable
-      // or after 30 seconds (whichever comes first).
-      registrationStrategy: 'registerWhenStable:30000'
-    })
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
